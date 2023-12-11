3545b441f80e606c068dafcea919ef18840aa238
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-12-11 12:00:53 -0800

    Lunch Break

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import CatEdit from "./pages/CatEdit"
@@ -13,18 +13,19 @@ import { Routes, Route } from "react-router-dom"
 import mockCats from "./mockCats"
 
 const App = () => {
+  const [cats, setCats] = useState(mockCats)
 
   return (
     <>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catindex" element={<CatIndex />} />
-          <Route path="/catshow" element={<CatShow />} />
-          <Route path="/catnew" element={<CatNew />} />
-          <Route path="/catedit" element={<CatEdit />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catindex" element={<CatIndex cats={cats}/>} />
+        {/* <Route path="/catshow/:id" element={<CatShow cats={cats}/>} /> */}
+        <Route path="/catnew" element={<CatNew />} />
+        <Route path="/catedit" element={<CatEdit />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CatShow = () => {
+const CatShow = ({ cats }) => {
     return (
         <div>
             CatShow
