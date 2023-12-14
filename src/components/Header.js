915c908bdcbb95128c85ae633091db8f7fa39449
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-12-14 15:01:28 -0800

    Media Queries Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Nav, NavItem } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 import catLogo from "../assets/OhMyCatLogoText.png"
 import newProfile from "../assets/CatNewProfileInverse.png"
-import cuddle from "../assets/Cuddle.png"
+import cuddle from "../assets/CuddleRev.png"
 import '../App.css'
 
 const Header = () => {
@@ -24,13 +24,13 @@ const Header = () => {
         </NavItem>
         <NavItem>
             <NavLink 
-            to="/catindex" className="nav-link"
+            to="/catindex" className="nav-link">
             <img
             src={cuddle}
             alt="Two Cats Cuddling"
             className="cuddle"
             />
-            <span className='cuddle-text'>&nbsp;Cuddle Finder</span>
+            <span className='header-text'>&nbsp;Cuddle</span><span className='galaxy'> Finder</span>
             </NavLink>
         </NavItem>
         <NavItem>
@@ -41,7 +41,7 @@ const Header = () => {
             alt="New Profile logo"
             className="cat-logo-icon"
             />
-            <span>&nbsp;New Mew</span>
+            <span className='galaxy'>&nbsp;New </span><span className='header-text'>Mew</span>
             </NavLink>
         </NavItem>
         </Nav>
